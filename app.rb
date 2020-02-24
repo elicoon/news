4204208072ad4b418211ded4109ccbce7e57b17e
elicoon <elicoon@gmail.com>
2020-02-24 22:32:20 +0000

    mid-way saving

--- app.rb
+++ app.rb
@@ -3,16 +3,45 @@ require "sinatra/reloader"
 require "geocoder"
 require "forecast_io"
 require "httparty"
+require "open-uri"
 def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
 # enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "2d31f2693209b31c67232a10ff2a1feb"
+
+# NewsAPI key and code is below
+url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=be45392a87674b5a9b2f298fb7074f50"
+news = HTTParty.get(url).parsed_response.to_hash
 
 get "/" do
   # show a view that asks for the location
+  view "ask"
 end
 
 get "/news" do
-  # do everything else
+  # do everything else:
+  # start with obtaining a location and turning it into array "lat_long" where the first
+  # number is the lat and the second number is the long
+@location = params["q"]
+location_search = Geocoder.search(@location)
+lat_long = location_search.first.coordinates # => [lat, long]
+"#{lat_long[0]} #{lat_long[1]}"
+
+# next, pass that location to darksky to obtain weather information in array "forecast"
+forecast = ForecastIO.forecast(lat_long[0],lat_long[1]).to_hash
+
+# define current forecast variables within the hash
+@current_temperature=forecast["currently"]["temperature"]
+@current_conditions=forecast["currently"]["summary"]
+@referenced_temperature=forecast["daily"]["data"] #note that this needs a loop to actually define which day we want, plus ["temperature"]
+@referenced_summary=forecast["daily"]["data"] #note that this needs a loop to actually define which day we want ["summary"]
+
+
+# dealing with time in forecast hash. Can use these variables for loops
+@today_day_of_week=Time.at(forecast["currently"]["time"]).strftime("%A")
+# @referenced_day_of_week=Time.at(forecast["daily"]["data"]["SPECIFY DAY LOOPS"]["time"]]).strftime("%A") 
+
+
+view "news"
 end
