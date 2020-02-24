b6b7a533ec35d474040238a5fdae541d495030b2
elicoon <elicoon@gmail.com>
2020-02-24 23:30:02 +0000

    weather forecast working

--- app.rb
+++ app.rb
@@ -23,9 +23,10 @@ get "/news" do
   # do everything else:
   # start with obtaining a location and turning it into array "lat_long" where the first
   # number is the lat and the second number is the long
-@location = params["q"]
-location_search = Geocoder.search(@location)
+@form_entry = params["q"]
+location_search = Geocoder.search(@form_entry)
 lat_long = location_search.first.coordinates # => [lat, long]
+@location_result_city=location_search.first.city
 "#{lat_long[0]} #{lat_long[1]}"
 
 # next, pass that location to darksky to obtain weather information in array "forecast"
@@ -34,9 +35,7 @@ forecast = ForecastIO.forecast(lat_long[0],lat_long[1]).to_hash
 # define current forecast variables within the hash
 @current_temperature=forecast["currently"]["temperature"]
 @current_conditions=forecast["currently"]["summary"]
-@referenced_temperature=forecast["daily"]["data"] #note that this needs a loop to actually define which day we want, plus ["temperature"]
-@referenced_summary=forecast["daily"]["data"] #note that this needs a loop to actually define which day we want ["summary"]
-
+@future_forecast=forecast["daily"]["data"] #note that this needs a loop to actually define which day we want, plus ["temperature"] or ["summary"] or ["time"]
 
 # dealing with time in forecast hash. Can use these variables for loops
 @today_day_of_week=Time.at(forecast["currently"]["time"]).strftime("%A")
